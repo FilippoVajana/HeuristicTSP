0c126c3b21403d107e4a051ee1e1d6c153a88621
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2019-12-06 21:28:15 +0100

    fix circuits plot

--- analysis.py
+++ analysis.py
@@ -3,16 +3,16 @@ import glob
 import matplotlib.pyplot as plt
 import numpy as np
 
-def read_cities_pos(file_path : str):
-    cities_pos_map = dict()
+def read_positions_file(file_path : str):
+    positions = dict()
     # parse file
     file = open(file_path, 'r')    
     for line in file:
         line = str(line).replace('\n', '')
         split = [float(s) for s in line.split(' ') if s != '']
-        cities_pos_map[int(split[0] - 1)] = (split[1], split[2])
+        positions[int(split[0] - 1)] = (split[1], split[2])
     
-    return cities_pos_map
+    return positions # {Node, (X,Y)}
 
 def read_results(file_path : str):
     results = []
@@ -24,34 +24,37 @@ def read_results(file_path : str):
         cost = int(lines[i+1].replace('\n', ''))
         results.append((circuit, cost))
     
-    return results
+    return results # [(circuit, cost)]
 
-def plot_solutions(cities_pos, result, ax):
+def plot_circuit(cities_pos, result, ax):
     circuit, cost = result
     # get cities coordinates
     pos = [v for v in cities_pos.values()]
     x = [p[0] for p in pos]
     y = [p[1] for p in pos] 
 
-    # plot cities    
-    # _, ax = plt.subplots()    
-    sc = ax.scatter(x,y) 
+    # plot cities
+    sc = ax.scatter(x,y)
+    
     for idx, p in enumerate(pos):        
         x.append(p[0])
         y.append(p[1])
         if idx == 0:       
-            ax.annotate(f"{idx} [start]", xy=(p[0],p[1]), xytext=(p[0],p[1] + 25))
+            ax.annotate(f"{idx} [start]", xy=(p[0],p[1]), xytext=(p[0],p[1]))
         else:
-            ax.annotate(f"{idx}", xy=(p[0],p[1]), xytext=(p[0],p[1] + 25)) 
+            ax.annotate(f"{idx}", xy=(p[0],p[1]), xytext=(p[0],p[1])) 
 
     # plot circuit
     for idx, node in enumerate(circuit):
         end = cities_pos[node]
         start = cities_pos[circuit[idx + 1]] if (idx < len(circuit) - 1) else cities_pos[circuit[0]]
-        ax.annotate("", xy=start, xycoords='data', xytext=end, textcoords='data', arrowprops=dict(arrowstyle="->", connectionstyle="arc3"))
+        ax.annotate("", xy=start, xycoords='data', xytext=end, textcoords='data', arrowprops=dict(arrowstyle="->", connectionstyle="arc3"))    
     
-    plt.draw()
     return ax
+
+def plot_instance_circuits(name : str, node_pos, results):
+    pass
+
 
 def plot_costs(costs):
     optimum = 1610   
@@ -95,28 +98,51 @@ def plot_costs(costs):
 
 
 if __name__ == '__main__':
-    print(os.getcwd())
-    cities_pos = read_cities_pos("./data/bayg29_pos.dat")
+    # get nodes positions
+    pos_root = "./data/instances"
+    pos_files = sorted(list(filter(lambda name: '_pos.dat' in name, os.listdir(pos_root))))
+    pos_dict = dict()
+    
+    for f in pos_files:        
+        pos = read_positions_file(os.path.join(pos_root, f))
+        pos_dict[str(f).replace("_pos.dat", "")] = pos
 
-    files = [os.path.normpath(f) for f in glob.glob("./data/results/" + "*.txt", recursive=False)]
-    files.sort()
-    results = read_results(files[-1])
 
-    best_res = min(results, key=lambda t: t[1])
-    best_res_idx = results.index(best_res)
-    worst_res = max(results, key=lambda t: t[1])
-    worst_res_idx = results.index(worst_res)
+    # get solutions circuits
+    res_root = "./data/results/6023"
+    res_files = sorted(os.listdir(res_root))
+    res_dict = dict()
 
-    print((best_res, best_res_idx))
-    print((worst_res, worst_res_idx))
+    for f in res_files:        
+        res = read_results(os.path.join(res_root, f))
+        res_dict[str(f).replace("_mat.dat.txt", "")] = res
 
-    _, ax = plt.subplots(1,2, False, True)
-    ax[0].set_title("Best Result")
-    ax[1].set_title("Worst Result")
-    best_ax = plot_solutions(cities_pos, results[best_res_idx], ax[0])
-    worst_ax = plot_solutions(cities_pos, results[worst_res_idx], ax[1])
+    print("Positions Files: ", pos_dict.keys())
+    print("Results Files: ", res_dict.keys())
 
-    costs = [c for (cir, c) in results]
-    plot_costs(costs)
+
+    # plot circuits
+    for k in sorted(pos_dict.keys()):
+        print(k)
+
+        fig, ax = plt.subplots(nrows=1, ncols=2)
+        fig.suptitle(k, size=22)
+        plt.setp(ax, xticks=[], yticks=[])
+        plt.subplots_adjust(wspace=0)
+
+        # get best and worst circuit        
+        best = min(res_dict[k], key=lambda t: t[1])
+        worst = max(res_dict[k], key=lambda t: t[1])
+
+        # plot        
+        plot_circuit(pos_dict[k], best, ax[0])
+        ax[0].set_title(f"value: {best[1]}")
+        plot_circuit(pos_dict[k], worst, ax[1])
+        ax[1].set_title(f"value: {worst[1]}")
 
     plt.show()
+
+    # costs = [c for (cir, c) in results]
+    # plot_costs(costs)
+
+    # plt.show()
