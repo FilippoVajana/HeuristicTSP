d69e241254edc6287157e268a085999531f81c00
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2019-12-11 21:50:54 +0100

    add Jupyter notebook for data analysis

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 import numpy as np
 
 def read_positions_file(file_path : str):
@@ -57,10 +58,10 @@ def plot_instance_circuits(name : str, node_pos, results):
 
 
 def plot_costs(costs, optimum, instance_name):      
-    fig = plt.figure()
+    fig = plt.figure(figsize=(10,10))
     fig.suptitle(instance_name, size=22)
 
-    ax1 = plt.subplot(311)
+    ax1 = plt.subplot(321)
     plt.hist(costs, 15, rwidth=.8)
     plt.xlabel('Solution value')
     plt.ylabel('Count')
@@ -68,7 +69,7 @@ def plot_costs(costs, optimum, instance_name):
     plt.axvline(x=optimum, label=f'optimum = {optimum}', c='red')
     plt.legend(frameon=False)
 
-    ax2 = plt.subplot(312)
+    ax2 = plt.subplot(322)
     plt.minorticks_on()
     plt.xlabel('Relative solution quality [%]')
     plt.ylabel('Cumulative frequency')
@@ -77,20 +78,54 @@ def plot_costs(costs, optimum, instance_name):
     diff_relative = np.abs(data - optimum) / optimum    
     plt.plot(diff_relative, np.arange(len(diff_relative)))
 
-    ax3 = plt.subplot(313)
-    plt.boxplot(costs, labels=['Benchmark instance'])
+    ax3 = plt.subplot(323)
+    plt.boxplot(costs, vert=False)
     for c in costs:
-        plt.scatter(x=1, y=c, c='silver')
+        plt.scatter(x=c, y=1, c='silver')
     plt.ylabel('Solution value')
     
-    plt.scatter(x=1, y=optimum, label=f'optimum = {optimum}', c='red')
+    plt.scatter(x=optimum, y=1, label=f'optimum = {optimum}', c='red')
     plt.plot([], [], label=f'median = {np.median(costs)}', c='orange')
     plt.plot([], [], label=f'min,max = {np.min(costs)},{np.max(costs)}')
     plt.plot([], [], label=f'Q1,Q3 = {np.quantile(costs, .25)},{np.quantile(costs, .75)}', c='black')    
     
     plt.legend(frameon=False)
 
+
+def plot_costs2(costs, optimum, instance_name):
+    fig = plt.figure(figsize=(10,10))
+    # fig.suptitle(instance_name, size=22)
+
+    gs = GridSpec(2, 2, width_ratios=[2, 1], height_ratios=[1, 1])
+
+    ax1 = fig.add_subplot(gs[0, 0:1])
+    plt.hist(costs, 15, rwidth=.8)
+    plt.xlabel('Solution value')
+    plt.ylabel('Count')
+    plt.minorticks_on()
+    plt.axvline(x=optimum, label=f'optimum = {optimum}', c='red')
+
+    ax2 = fig.add_subplot(gs[1, 0:1])
+    plt.minorticks_on()
+    plt.xlabel('Relative solution quality [%]')
+    plt.ylabel('Cumulative frequency')    
+    costs.sort()
+    data = np.array(costs)
+    diff_relative = np.abs(data - optimum) / optimum * 100
+    plt.plot(diff_relative, np.arange(len(diff_relative)))
+
+
+    ax3 = fig.add_subplot(gs[:, -1])    
+    plt.boxplot(costs, labels=[""])
+    for c in costs:
+        plt.scatter(x=1, y=c, c='silver')    
     
+    plt.scatter(x=1, y=optimum, label=f'optimum = {optimum}', c='red')
+    plt.plot([], [], label=f'median = {np.median(costs)}', c='orange')
+    plt.plot([], [], label=f'min,max = {np.min(costs)},{np.max(costs)}')
+    plt.plot([], [], label=f'Q1,Q3 = {np.quantile(costs, .25)},{np.quantile(costs, .75)}', c='black')    
+    
+    return fig
 
 
 
@@ -121,29 +156,28 @@ if __name__ == '__main__':
 
 
     # plot circuits
-    for k in sorted(pos_dict.keys()): 
-        fig, ax = plt.subplots(nrows=1, ncols=2)
-        fig.suptitle(k, size=22)
-        plt.setp(ax, xticks=[], yticks=[])
-        plt.subplots_adjust(wspace=0)
+    # for k in sorted(pos_dict.keys()): 
+    #     fig, ax = plt.subplots(nrows=1, ncols=2)
+    #     fig.suptitle(k, size=22)
+    #     plt.setp(ax, xticks=[], yticks=[])
+    #     plt.subplots_adjust(wspace=0)
 
-        # get best and worst circuit        
-        best = min(res_dict[k], key=lambda t: t[1])
-        worst = max(res_dict[k], key=lambda t: t[1])
+    #     # get best and worst circuit        
+    #     best = min(res_dict[k], key=lambda t: t[1])
+    #     worst = max(res_dict[k], key=lambda t: t[1])
 
-        # plot        
-        plot_circuit(pos_dict[k], best, ax[0])
-        ax[0].set_title(f"value: {best[1]}")
-        plot_circuit(pos_dict[k], worst, ax[1])
-        ax[1].set_title(f"value: {worst[1]}")
+    #     # plot        
+    #     plot_circuit(pos_dict[k], best, ax[0])
+    #     ax[0].set_title(f"value: {best[1]}")
+    #     plot_circuit(pos_dict[k], worst, ax[1])
+    #     ax[1].set_title(f"value: {worst[1]}")
     # plt.show()
     
     # plot costs
     opt = {
         "att48" : 10628,
         "bayg29" : 1610,
-        "bays29" : 2020,
-        "berlin52" : 7542,
+        "bays29" : 2020,        
         "burma14" : 3323,
         "fri26" : 937,
         "gr21" : 2707,
@@ -152,7 +186,9 @@ if __name__ == '__main__':
         "st70" : 675 }
 
     for k in sorted(res_dict.keys()):
-        costs = [c for (circuit, c) in res_dict[k]]        
-        plot_costs(costs, opt[k], k)
+        costs = [c for (circuit, c) in res_dict[k]]
+        fig = plot_costs2(costs, opt[k], k)
+        plt.savefig(f"./data/results/plots/{k}.png", arr=fig, format='png')
+        # plt.show()
         
-    plt.show()
+    # plt.show()
